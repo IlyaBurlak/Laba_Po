f6d83229c07b5c8468c9761ad5affd471095bd83
Ilya Burlak <113059204+IlyaBurlak@users.noreply.github.com>
2022-09-25 14:34:27 +0300

    Start

---
+++ git_Ners/git_Ners/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  git_Ners
+//
+//  Created by Илья Бурлак on 25.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  learning_C
+//
+//  Created by Илья Бурлак on 06.09.2022.
+//
+
+#include "main.h"
+int main(){
+    const len = 5 ;
+    int arr[len] = {100 ,30 ,20 ,99 ,79};
+    
+    
+    for (int i = 0; i < len ; i++ ){
+        printf("%d" , arrq[i])
+    }
+    
+    
+    return 0;
+}
